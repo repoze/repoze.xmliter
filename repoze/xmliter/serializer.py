1b854943353298f7d75d07c7fae7540ba7d7bfdd
Laurence Rowe <l@lrowe.co.uk>
2011-06-03 15:33:58 +0100

    Add doctype option to replace doctype on serialization.

--- repoze/xmliter/serializer.py
+++ repoze/xmliter/serializer.py
@@ -1,22 +1,38 @@
 import lxml.etree
+import re
+
+doctype_re = re.compile("<!DOCTYPE\s[^>]+>", re.MULTILINE)
 
 class XMLSerializer(object):
     
-    def __init__(self, tree, serializer=lxml.etree.tostring, pretty_print=False):
+    def __init__(self, tree, serializer=None, pretty_print=False, doctype=None):
+        if serializer is None:
+            serializer = lxml.etree.tostring
         self.tree = tree
         self.serializer = serializer
         self.pretty_print = pretty_print
-        
+        self.doctype = doctype
+
+    def serialize(self, encoding=None):
+        # Defer to the xsl:output settings if appropriate
+        if isinstance(self.tree, lxml.etree._XSLTResultTree):
+            if encoding is unicode:
+                result = unicode(self.tree)
+            else:
+                result = str(self.tree)
+        else:
+            result = self.serializer(self.tree, encoding=encoding, pretty_print=self.pretty_print)
+        if self.doctype is not None:
+            result, subs = doctype_re.subn(self.doctype, result, 1)
+            if not subs:
+                result = self.doctype + result
+        return result
+
     def __iter__(self):
         return iter(str(self),)
 
     def __str__(self):
-        # Defer to the xsl:output settings if appropriate
-        if isinstance(self.tree, lxml.etree._XSLTResultTree):
-            return str(self.tree)
-        return self.serializer(self.tree, pretty_print=self.pretty_print)
+        return self.serialize()
 
     def __unicode__(self):
-        if isinstance(self.tree, lxml.etree._XSLTResultTree):
-            return unicode(self.tree)
-        return self.serializer(self.tree, encoding=unicode, pretty_print=self.pretty_print)
+        return self.serialize(unicode)
