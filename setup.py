3223eb482c3f224b2b39bae6bc60016c1b63372d
Tres Seaver <tseaver@palladion.com>
2022-01-14 11:38:12 -0500

    Prep 0.6.1 release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.6.1.dev0'
+__version__ = '0.6.1'
 
 import os
 from setuptools import setup, find_packages
