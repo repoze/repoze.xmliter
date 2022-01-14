5125fde1d68fb893b857491f02705e7afea82a2a
Maurits van Rees <maurits@vanrees.org>
2022-01-14 09:21:57 +0100

    Fixed tests with lxml 4.7.1 or higher.
    Fixes https://github.com/repoze/repoze.xmliter/issues/8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.6'
+__version__ = '0.6.1.dev0'
 
 import os
 from setuptools import setup, find_packages
